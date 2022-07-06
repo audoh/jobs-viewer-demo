2e236720e8a336d2b78590c2fbf5870ec61f2345
Ashleigh Udoh <ash@audoh.co.uk>
2022-07-06 23:18:20 +0100

    frontend hello world

---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve( __dirname, 'www' ),
+    filename: 'app.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./web/index.html",
+    }),
+  ],
+  resolve: {
+    modules: [__dirname, "src", "node_modules"],
+    extensions: ["*", ".js", ".jsx", ".tsx", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.png|svg|jpg|gif$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
